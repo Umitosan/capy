ed1da08c7695bdac6f1a38674197f24c889310ba
Dominic Brown <yeahdom@gmail.com>
2017-07-28 13:37:39 -0700

    add more ref

---
+++ reference/notes.rb
@@ -0,0 +1,25 @@
+# https://github.com/gosu/gosu/wiki/Window-Main-Loop
+
+# good EXAMPLE
+# https://www.thoughtco.com/mouse-and-keyboard-input-in-gosu-2908025
+
+
+# gem install gosu-examples
+# gosu-examples
+# You can also download and unpack this repository and then run individual examples from the terminal:
+#
+# cd gosu-examples-master/examples
+# ruby tutorial.rb
+# Some examples require the following additional libraries:
+#
+# gem install chipmunk
+# gem install rmagick
+# gem install opengl
+
+# WIKI
+# https://github.com/gosu/gosu/wiki
+
+
+# Animations from forum
+
+# https://www.libgosu.org/cgi-bin/mwf/topic_show.pl?tid=10
